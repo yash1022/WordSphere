765781aa2cc259d755891ead3fc274216719fa78
yash1022 <dyashsingh.4@gmail.com>
2024-11-01 19:18:15 +0530

    profilepage done

--- backend/config/firebaseconfig.js
+++ backend/config/firebaseconfig.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./blogg-e6a1b-firebase-adminsdk-qikns-3b55e9c4a1.json");
+const serviceAccount = require("./firebaseadk.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
